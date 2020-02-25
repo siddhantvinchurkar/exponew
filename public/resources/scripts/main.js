dfec7756a7c2b86068cbf3b448f2242854c38e94
Siddhant <siddhantvinchurkar@gmail.com>
2020-02-25 20:55:47 +0530

    Fixed Stuff

--- public/resources/scripts/main.js
+++ public/resources/scripts/main.js
@@ -37,8 +37,11 @@ window.onload = function () {
             $('#signed-in-content').css('display', 'block');
             db.collection('products').get().then(function (querySnapshot) {
                 querySnapshot.forEach(function (doc) {
-                    $('#card-holder').append('<div class="col s12 m12 l4 xl4"><div class="card"><div class="card-image"> <img class="materialboxed lazyload" data-caption="This furniture is the best!" src="' + doc.data().product_image + '"> <a id="' + doc.id + '" class="tooltipped btn - floating btn - large halfway - fab waves - effect waves - light red" data-position="bottom" data-tooltip="Add to cart"><i class="material - icons">add_shopping_cart</i></a></div><div class="card - content"><h3>' + doc.data().product_name + '</h3><h5> <strike style="font - size: 0.75em; ">₹ ' + doc.data().product_price + '</strike> <b>₹ ' + doc.data().product_price_dsctd + '</b></h5><p>' + doc.data().product_description + '</p></div></div></div>');
+                    $('#card-holder').append('<div class="col s12 m12 l4 xl4"><div class="card"><div class="card-image"> <img class="materialboxed lazyload" data-caption="This furniture is the best!" src="' + doc.data().product_image + '"> <a id="' + doc.id + '" class="right tooltipped btn-floating btn-large halfway-fab waves effect waves light red" data-position="bottom" data-tooltip="Add to cart"><i class="material-icons">add_shopping_cart</i></a></div><div class="card-content"><h3>' + doc.data().product_name + '</h3><h5> <strike style="font-size: 0.75em; ">₹ ' + doc.data().product_price + '</strike> <b>₹ ' + doc.data().product_price_dsctd + '</b></h5><p>' + doc.data().product_description + '</p></div></div></div>');
                 });
+                $('.tooltipped').tooltip();
+                $('.materialboxed').materialbox();
+
             }).catch(function (error) { console.error(error); });
         }).catch(function (error) {
             alert('Sign in unsuccessful!');
